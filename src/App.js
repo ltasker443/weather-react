00374a215afd24b3d48fbe28b579e645f22c958a
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-08-13 17:18:45 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     <h2 className="header">Current Weather</h2>
         <Search />
+
         <p id="github-link">
-      <a
-        href="https://github.com/ltasker443/weather-react"
-        target="blank"
-        id="github-link"
-        >Open-source</a> by Laura Tasker
-    </p>
+          <a
+            href="https://github.com/ltasker443/weather-react"
+            target="blank"
+            id="github-link"
+            >Open-source</a> by Laura Tasker
+      </p>
       </div>
   );
 }
